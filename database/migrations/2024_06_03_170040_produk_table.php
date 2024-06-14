bf112d782119496ad25f285bb4614835bcad2752
frllelrtza <alfarrelelmaretza@gmail.com>
2024-06-14 09:57:42 +0700

    [FIX] table name

--- database/migrations/2024_06_03_170040_produk_table.php
+++ database/migrations/2024_06_03_170040_produk_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Produk', function (Blueprint $table) {
+        Schema::create('produk', function (Blueprint $table) {
             $table->id();
             $table->string('nama_produk');
             $table->string('photo_produk');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Produk');
+        Schema::dropIfExists('produk');
     }
 };
